b1858146308a511d1e03a1e4eb956c5ff8a0a60d
GotTank <sibumihir1234@gmail.com>
2023-01-08 13:02:09 +0530

    add file via upload

--- JsFile.js
+++ JsFile.js
@@ -32,3 +32,55 @@ window.onclick = function(event) {
     }
   }
 }
+
+function myFun_for_android() {
+  document.getElementById("myproject-android").classList.toggle("show1");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn_Android')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content-android");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show1')) {
+        openDropdown.classList.remove('show1');
+      }
+    }
+  }
+}
+function myFun_for_web() {
+  document.getElementById("myproject-web").classList.toggle("show1");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn_web')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content-web");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show1')) {
+        openDropdown.classList.remove('show1');
+      }
+    }
+  }
+}
+function myFun_for_desktop() {
+  document.getElementById("myproject-desktop").classList.toggle("show1");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn_desktop')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content-desktop");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show1')) {
+        openDropdown.classList.remove('show1');
+      }
+    }
+  }
+}
